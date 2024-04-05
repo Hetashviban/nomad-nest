926052a6d8769abb549008239f13a9b20d00a562
Hetashvi Ban <hetashvi7988@gmail.com>
2024-04-05 15:47:34 -0400

    style: REST API pages

--- models/Listing.js
+++ models/Listing.js
@@ -27,17 +27,17 @@ const ListingSchema = new mongoose.Schema({
     },
     image: {
         type: String, // Specifies that the 'image' field should be a string
-    
+
         // If the image field is not defined or if it's empty then we set a default value for it.
-        default: "https://unsplash.com/photos/a-boat-is-in-a-large-body-of-water-ciYOQZ7eKQ4",
-    
+        default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
         // 'set' is a function that manipulates the value of 'image' before it's saved to the database\
         //It checks if the value is blank - if it is then it will be set to the url that is defined below
-        set: (v) => 
-        //The below three lines of code is using ternary  operator
+        set: (v) =>
+            //The below three lines of code is using ternary  operator
             v === "" // Check if the provided value is an empty string
-            ? "https://unsplash.com/photos/a-window-with-a-view-of-a-lake-and-mountains-qHeCtaaeT3g" // If empty, use the default URL
-            : v // Otherwise, keep the provided value
+                ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" // If empty, use the default URL
+                : v // Otherwise, keep the provided value
     }
 });
 
